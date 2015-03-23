86bc9cc61191694d8448b70c5f1ccac6bf996631
omahxyd <amoraleshe@gmail.com>
2015-03-23 14:25:07 +0100

    Merge branch 'Branch1'
    Conflicts:
    	app/src/main/java/com/example/andres/piechart/MainActivity.java

--- app/src/main/java/com/example/andres/piechart/MainActivity.java
+++ app/src/main/java/com/example/andres/piechart/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-//Merge result
+//Branch master
 public class MainActivity extends Activity {
 
     @Override
